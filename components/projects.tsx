0e0b2d7181b94dc2cdca44e2b31405aaa4d722b5
Hugo Dahl <hugodahl99@hotmail.com>
2023-03-16 17:57:23 +0100

    scrollIntoView for first button

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,8 +5,10 @@ const Projects = () => {
   const dataArr = null; //create json file and import
 
   return (
-    <div className="w-fit h-screen grid grid-cols-12 grid-rows-6">
-      
+    <div className="w-fill h-screen grid grid-cols-12 grid-rows-6">
+      <div className="col-span-9 col-start-3 row-start-2">
+        <p className="text-lg">dfjasldfsjkasadfasdf</p>
+      </div>
     </div>
   );
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Home from '../components/home';
 import Projects from '../components/projects';
 import ContactMe from '../components/contactMe';
+import React from 'react';
 
 function App() {
 
+  const homeRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
+   
   return (
     <div className="App m-5 ">
       <div className="fixed flex flex-col gap-4" >
-        <a href="#"><p className="underline text-xl hover:no-underline">home</p></a>
-        <a href="#"><p className="underline text-xl hover:no-underline" >projects</p></a>
-        <a href="#"><p className="underline text-xl hover:no-underline">contact me</p></a>
+        <a href="#home" onClick={() => {
+          if(homeRef && homeRef.current) {
+            homeRef.current.scrollIntoView({behavior:'smooth', block: 'start'})
+          }
+        }}><p className="underline text-xl hover:no-underline">home</p></a>
+        <a href="#projects"><p className="underline text-xl hover:no-underline" >projects</p></a>
+        <a href="#contactinfo"><p className="underline text-xl hover:no-underline">contact me</p></a>
       </div>
-      <div className='static'>
+      <div ref={homeRef} className='static'>
         <Home/>
       </div>
-      <div className='static'>
+      <div ref={projectsRef} className='static'>
         <Projects/>
       </div>
-      <div className='static'>
+      <div ref={contactRef} className='static'>
         <ContactMe/>
       </div>
     </div>
   )
 }
+
 
 export default App
